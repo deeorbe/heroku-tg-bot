b14de129fa7fae8ef0f3c9ae3604962dcf3415bf
deeorbe <57874450+deeorbe@users.noreply.github.com>
2020-06-23 13:10:47 +0500

    initial

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '1189037689:AAEvSk5bVm_udSK-niuXBw40YbZCop874PI';
-
-
-console.log('Bot has been started . . .')
-const bot = new TelegramBot(token, { polling: true });
-
-
-
-bot.on('message',msg=>{
-
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId,`Hello ${msg.from.first_name} !`)
-})
+require('./bot');
+require('./web');
---
+++ web.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let packageInfo = require('./package.json');
+
+let app = express();
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+let server = app.listen(process.env.PORT, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Web server started at http://%s:%s', host, port);
+});
